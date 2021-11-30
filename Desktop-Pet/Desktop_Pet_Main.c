2a7c48bf389e45670f370e53baaa63bdd0ca1b94
randomuserhi <magicalworld.nano@gmail.com>
2021-11-30 02:41:13 +0000

    Created transparent window that sits ontop of desktop.

--- Desktop-Pet/Desktop_Pet_Main.c
+++ Desktop-Pet/Desktop_Pet_Main.c
@@ -4,34 +4,80 @@
 #include "Deep_Engine.h"
 #include "Deep_String.h"
 
-#include <time.h>
-#include <signal.h>
+// https://docs.microsoft.com/en-us/cpp/porting/modifying-winver-and-win32-winnt?view=msvc-170
+#define _WIN32_WINNT_WIN10 0x0A00
+#include <Windows.h>
 
-static volatile int running = DEEP_TRUE;
+#include <d3d12.h>
 
-void closeSignalHandler(int dummy) {
-	running = DEEP_FALSE;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+    case WM_CHAR:
+        if (wparam == VK_ESCAPE)
+        {
+            DestroyWindow(hwnd);
+        }
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hwnd, message, wparam, lparam);
+    }
 }
 
 int main()
 {
-	signal(SIGINT, closeSignalHandler);
-
 #ifdef DEEP_DEBUG_MEMORY
 	Deep_Debug_Memory_Start();
 #endif
 
-	struct Deep_Vector3 vec3 = Deep_Vector3_Create(10, 10, 0);
-	Deep_Vector3_Scale_InPlace(&vec3, 2);
-	printf("%f %f %f\n", vec3.x, vec3.y, vec3.z);
+	//HWND hWnd = GetConsoleWindow();
+	//ShowWindow(hWnd, SW_MINIMIZE);
+	//ShowWindow(hWnd, SW_HIDE);
 
-	while(running)
-	{
+    const char* windowClassName = "Window in Console";
+    WNDCLASS windowClass = { 0 };
+    windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    windowClass.hInstance = NULL;
+    windowClass.lpfnWndProc = WndProc;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    if (!RegisterClass(&windowClass))
+        MessageBox(NULL, "Could not register class", "Error", MB_OK);
 
-	}
+    HWND windowHandle = CreateWindowEx(
+        WS_EX_COMPOSITED | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST,
+        windowClassName,
+        NULL,
+        WS_POPUP, //borderless
+        0, //x coordinate of window start point
+        0, //y start point
+        GetSystemMetrics(SM_CXSCREEN), //width of window; this function
+        GetSystemMetrics(SM_CYSCREEN), //height of the window
+        NULL, //handles and such, not needed
+        NULL,
+        NULL,
+        NULL);
+    ShowWindow(windowHandle, SW_MAXIMIZE);
+
+    SetLayeredWindowAttributes(windowHandle, 0, 100, LWA_ALPHA);
+
+    MSG messages;
+    while (GetMessage(&messages, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&messages);
+        DispatchMessage(&messages);
+    }
+    DeleteObject(windowHandle); //doing it just in case
 
 #ifdef DEEP_DEBUG_MEMORY
-	Deep_Debug_Memory_End();
+    Deep_Debug_Memory_End();
 #endif
+
+    return messages.wParam;
 }
 
